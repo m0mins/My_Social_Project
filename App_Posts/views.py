67ae009b8f4bc7ea0894e20bca4a302ad789460d
m0mins <momin.pstu.cse11.bd@gmail.com>
2023-09-25 17:48:08 +0600

    post a photo and edit photo done

--- App_Posts/views.py
+++ App_Posts/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from django.contrib.auth.decorators import login_required
 
+from App_Login.models import UserProfile
+from django.contrib.auth.models import User
+
 # Create your views here.
 @login_required
 def home(request):
-    return render(request,'App_Posts/home.html',context={'title':'Home'})
+    if request.method=='GET':
+        search=request.GET.get('search')
+        result=User.objects.filter(username=search)
+    return render(request,'App_Posts/home.html',context={'title':'Home','search':search,'result':result})
