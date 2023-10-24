8961b0e6f762eadea943c923e43ac1f1f3f3a1bb
m0mins <momin.pstu.cse11.bd@gmail.com>
2023-10-25 01:52:28 +0600

    modified readme and gitignore

--- My_Social_Project/settings.py
+++ My_Social_Project/settings.py
@@ -72,11 +72,15 @@ WSGI_APPLICATION = 'My_Social_Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+DATABASES={
+   'default':{
+      'ENGINE':'django.db.backends.postgresql_psycopg2',
+      'NAME':'SocialAppDB',
+      'USER':'postgres',
+      'PASSWORD':'Pass@99',
+      'HOST':'127.0.0.1',
+      'PORT':'5432',
+   }
 }
 
 
---
+++ README.md
@@ -0,0 +1,4 @@
+# Python Version
+create a virtual environemt using python 3.10.6
+
+run -> pip install -r requiremtents.txt
